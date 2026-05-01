c97cc871ee69be5220f0e459398eb288afdf454a
Everett Bergeson <everett.bergeson@awardco.com>
2026-05-01 10:00:23 -0600

    Update frontend

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,5 +1,6 @@
 // API configuration
-export const API_URL = 'http://127.0.0.1:8000';
+// export const API_URL = 'http://127.0.0.1:8000';
+export const API_URL = 'https://simple-website-7opr.onrender.com';
 
 export function getAuthHeader() {
   const token = localStorage.getItem('token');
